0cbb4a654962773edb0290816d814d213bbfc042
varun8177 <varunergurala9999@gmail.com>
2023-05-20 12:51:27 +0530

    added: Initial folder structure of react

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Register from './Pages/Register';
+
+function App() {
+  return (
+    <div className="App">
+      <Register />
+    </div>
+  );
+}
+
+export default App;
