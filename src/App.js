1fc5a8267775132ce0f00ce3284cd254f1d35e69
varun8177 <varunergurala9999@gmail.com>
2023-05-20 18:52:28 +0530

    added: react-router dom for navigation

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import Home from './Pages/Home';
 import Login from './Pages/Login';
 import Register from './Pages/Register';
+import { Route, Routes } from 'react-router-dom';
 import './style.scss'
 function App() {
+  const routes = [
+    { path: "/", element: <Home /> },
+    { path: "/register", element: <Register /> },
+    { path: "/login", element: <Login /> }
+  ]
   return (
     <div className="App">
-      <Register />
+      <Routes>
+        {routes.map((item) => {
+          return <Route key={item.path} path={item.path} element={item.element} />
+        })}
+      </Routes>
     </div>
   );
 }
--- src/Pages/Register.jsx
+++ src/Pages/Register.jsx
@@ -4,9 +4,12 @@ import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
 import { auth, db, storage } from "../firebase";
 import { ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
 import { doc, setDoc } from "firebase/firestore";
+import { useNavigate } from "react-router-dom";
 
 const Register = () => {
   const [err, setError] = useState(false);
+  const navigate = useNavigate();
+
   const handleSubmit = async (e) => {
     e.preventDefault();
     const display = e.target[0].value;
@@ -15,8 +18,10 @@ const Register = () => {
     const file = e.target[3].files[0];
 
     try {
+      // Signup a new user using firebase
       const res = await createUserWithEmailAndPassword(auth, email, password);
       if (res.user) {
+        // store his profile photo and display name
         const storageRef = ref(storage, display);
         const uploadTask = uploadBytesResumable(storageRef, file);
 
@@ -33,17 +38,25 @@ const Register = () => {
             getDownloadURL(uploadTask.snapshot.ref).then(
               async (downloadURL) => {
                 if (res.user) {
+                  // update profile with display name & profile photo
                   await updateProfile(res.user, {
                     displayName: display,
                     photoURL: downloadURL,
                   });
+
+                  // create a collection of user that will contain user's data
                   await setDoc(doc(db, "users", res.user.uid), {
                     uid: res.user.uid,
                     displayName: display,
                     email,
                     photoURL: downloadURL,
                   });
+
+                  // create a collection of user chats that'll contain all messages of a particular user
                   await setDoc(doc(db, "userChats", res.user.uid));
+
+                  // navigate to chat page
+                  navigate("/");
                 }
               }
             );
